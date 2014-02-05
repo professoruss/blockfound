c2ec96767349d4c305f99cea62e78b92744c285e
Russ Johnson <russ@angrypacket.com>
2014-02-05 15:24:11 -0800

    added support for pushover notifications

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-Figure out when slush pool finds a new block.
-I suck at coding
+Blockfound
+=========
+
+Blockfound will notify you when Slush's pool finds a new block
+  - Ruby
+  - Support for [Pushover] - ios/android push notifications
+
+Version
+----
+
+1.0
+
+
+Installation
+--------------
+
+```sh
+gem install json
+gem install yaml
+git clone https://github.com/professoruss/blockfound.git
+```
+
+
+Run the damn thing!
+---------------
+output to console
+```sh
+ruby blockfound.rb
+```
+
+Push Notifications Via pushover
+-------------------------------
+enter your token and user into the yaml file
+```sh
+cd blockfound
+vim pushover.yaml
+```
+push notifications
+```sh
+ruby blockfound.rb pushover
+```
+
+License
+----
+
+MIT
+
+
+[Pushover]:https://pushover.net
+    
--- blockfound.rb
+++ blockfound.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'json'
 require 'open-uri'
+require 'yaml'
+require 'net/https'
 
   result = JSON.load(open("http://mining.bitcoin.cz/stats/json/"))
 
@@ -25,7 +27,24 @@ require 'open-uri'
   lastround = blocks[foo]
   #get how long the last round took
   duration = lastround["mining_duration"]
+  message = "Found new block!! Elapsed: #{duration} Hashrate: #{gh}GH/s"
+  if(b > strpone)
+    #pushover notifications
+    if ARGV[0] == 'pushover'
+      yaml = YAML.load_file('pushover.yaml')
+      url = URI.parse("https://api.pushover.net/1/messages.json")
+      req = Net::HTTP::Post.new(url.path)
+      req.set_form_data({
+        :token => "#{yaml['token']}",
+        :user => "#{yaml['user']}",
+        :message => "#{message}",
+      })
+      res = Net::HTTP.new(url.host, url.port)
+      res.use_ssl = true
+      res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      res.start {|http| http.request(req) }
 
-  if(b < strpone)
-    puts "Found new block!! Elapsed: #{duration} Hashrate: #{gh}GH/s"
+      exit
+    end
+    puts message
   end
